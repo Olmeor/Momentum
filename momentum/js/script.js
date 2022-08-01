da43238e40660f14c688a5e6edb9ea334fd47374
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-01 07:34:14 +0700

    feat: add weather vidget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -51,23 +51,27 @@ const forename = document.querySelector('.name');
 
 function setLocalStorage() {
   localStorage.setItem('name', forename.value);
+  localStorage.setItem('city', city.value ?? 'Minsk');
 }
 
 function getLocalStorage() {
-  if(localStorage.getItem('name')) {
+  if (localStorage.getItem('name')) {
     forename.value = localStorage.getItem('name');
+  }
+  if (localStorage.getItem('city')) {
+    city.value = localStorage.getItem('city');
   }
 }
 
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 
-// Slider
+// Background slider
 
 let randomNum = getRandomNum();
 const body = document.body;
-const slideNext = document.querySelector('.slide-prev');
-const slidePrev = document.querySelector('.slide-next');
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 
 function getRandomNum(num = 20) { // num - количество картинок
   return Math.floor(Math.random() * num + 1);
@@ -93,6 +97,54 @@ function getSlidePrev() {
   setBg(randomNum);
 }
 
-setBg();
+setBg(randomNum);
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+
+// Weather
+
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherTemperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const weatherWind = document.querySelector('.wind');
+const weatherHumidity = document.querySelector('.humidity');
+const weatherError = document.querySelector('.weather-error');
+let city = document.querySelector('.city');
+
+
+async function getWeather() {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${(city.value || localStorage.getItem('city')) ?? 'Minsk'}&lang=en&appid=7153241524dddce83603c2b94a1ad19c&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.weather[0].id, data.weather[0].description, data.main.temp, city.value);
+    weatherError.textContent = ``;
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    weatherTemperature.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherWind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    weatherHumidity.textContent = `Humidity: ${Math.round(data.main.humidity)}%`;
+    weatherDescription.textContent = data.weather[0].description;
+  } catch {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${(city.value || localStorage.getItem('city')) ?? 'Minsk'}&lang=en&appid=7153241524dddce83603c2b94a1ad19c&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = '';
+    weatherTemperature.textContent = ``;
+    weatherWind.textContent = ``;
+    weatherHumidity.textContent = ``;
+    weatherDescription.textContent = ``;
+    if (data.cod == 404) {
+      weatherError.textContent = `Error! City not found!`;
+    } else if (data.cod == 400) {
+      weatherError.textContent = `Nothing to geocode!`;
+      // window.localStorage.removeItem('city');
+      // city.value = 'Minsk';
+      if (!localStorage.getItem('city')) city.value = 'Minsk';
+    }
+
+  }
+}
+
+getWeather();
+city.addEventListener('change', getWeather);
