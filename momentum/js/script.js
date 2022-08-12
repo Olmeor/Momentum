10a348d784ca8565ac2336aceb9ff26b4bd8d934
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-13 01:50:46 +0700

    fix: abort todo

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -511,9 +511,6 @@ const greetingBlock = document.querySelector('.greeting-container');
 const quotesBlock = document.querySelector('.quote-wrapper');
 const playerBlock = document.querySelector('.player');
 const weatherBlock = document.querySelector('.weather');
-const todoMenu = document.querySelector('.todo');
-const todoButton = document.querySelector('.todo-button');
-
 
 let userSettings = {
   languageButton: false,
@@ -530,11 +527,6 @@ let objChecked = JSON.parse(localStorage.getItem('settings')) ?? userSettings;
 function toggleSettingsMenu() {
   settingsForm.classList.toggle('settings-open');
   settingsMenu.classList.toggle('settings-menu-shadow');
-}
-
-function toggleTodoMenu() {
-  todoMenu.classList.toggle('hidden-block'); //disable-block
-  // settingsMenu.classList.toggle('settings-menu-shadow');
 }
 
 function toggleSettings() {
@@ -598,7 +590,6 @@ setCheckedSettings();
 setSettingLang();
 
 settingsButton.addEventListener('click', toggleSettingsMenu);
-todoButton.addEventListener('click', toggleTodoMenu);
 settingsMenu.addEventListener('click', (e) => {
   if (e.target === settingsMenu) {
     toggleSettingsMenu();
@@ -608,74 +599,6 @@ settingsForm.addEventListener("change", (e) => {
   // console.log('пип настройки'); // проверка окна настроек
   toggleSettings();
 })
-
-// Todo
-
-const todo = {
-  action(e) {
-    const target = e.target;
-    if (target.classList.contains('todo__action')) {
-      const action = target.dataset.todoAction;
-      const elemItem = target.closest('.todo__item');
-      if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-        elemItem.remove();
-      } else {
-        elemItem.dataset.todoState = action;
-        const lexicon = {
-          active: 'восстановлено',
-          completed: 'завершено',
-          deleted: 'удалено'
-        };
-        const elTodoDate = elemItem.querySelector('.todo__date');
-        const html = `<span>${lexicon[action]}: ${new Date().toLocaleString().slice(0, -3)}</span>`;
-        elTodoDate.insertAdjacentHTML('beforeend', html);
-      }
-      this.save();
-    } else if (target.classList.contains('todo__add')) {
-      this.add();
-      this.save();
-    }
-  },
-  add() {
-    const elemText = document.querySelector('.todo__text');
-    if (elemText.disabled || !elemText.value.length) {
-      return;
-    }
-    document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value));
-    elemText.value = '';
-  },
-  create(text) {
-    const date = JSON.stringify({ add: new Date().toLocaleString().slice(0, -3) });
-    return `<li class="todo__item" data-todo-state="active">
-      <span class="todo__task">
-        ${text}
-        <span class="todo__date" data-todo-date="${date}">
-          <span>добавлено: ${new Date().toLocaleString().slice(0, -3)}</span>
-        </span>
-      </span>
-      <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-      <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-      <span class="todo__action todo__action_delete" data-todo-action="deleted"></span></li>`;
-  },
-  init() {
-    const fromStorage = localStorage.getItem('todo');
-    if (fromStorage) {
-      document.querySelector('.todo__items').innerHTML = fromStorage;
-    }
-    document.querySelector('.todo__options').addEventListener('change', this.update);
-    document.addEventListener('click', this.action.bind(this));
-  },
-  update() {
-    const option = document.querySelector('.todo__options').value;
-    document.querySelector('.todo__items').dataset.todoOption = option;
-    document.querySelector('.todo__text').disabled = option !== 'active';
-  },
-  save() {
-    localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
-  }
-};
-
-todo.init();
 
 // Import
 
