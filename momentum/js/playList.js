dd9b7c5ed949a4801f0e20816d1c78e902af3684
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-21 01:37:22 +0700

    feat: change music

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,28 +1,38 @@
 const playList = [
   {
-    title: 'Aqua Caelestis',
-    src: './assets/sounds/Aqua Caelestis.mp3',
-    duration: 39,
+    title: 'ME-Vigil',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME-Vigil.mp3',
+    duration: 104,
   },
   {
-    title: 'Ennio Morricone',
-    src: './assets/sounds/Ennio Morricone.mp3',
-    duration: 97,
+    title: 'ME-Uncharted_Worlds',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME-Uncharted_Worlds.mp3',
+    duration: 224,
   },
   {
-    title: 'River Flows In You',
-    src: './assets/sounds/River Flows In You.mp3',
-    duration: 97,
+    title: 'ME2-Afterlife',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME2-Afterlife.mp3',
+    duration: 297,
   },
   {
-    title: 'Summer Wind',
-    src: './assets/sounds/Summer Wind.mp3',
-    duration: 110,
+    title: 'ME2-New_Worlds',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME2-New_Worlds.mp3',
+    duration: 150,
   },
   {
-    title: 'The X-Files',
-    src: './assets/sounds/The X-Files.mp3',
-    duration: 41,
+    title: 'ME2-The_Illusive_Man',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME2-The_Illusive_Man.mp3',
+    duration: 142,
+  },
+  {
+    title: 'ME3-Leaving_Earth',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME3-Leaving_Earth.mp3',
+    duration: 122,
+  },
+  {
+    title: 'ME3-Das_Malefitz',
+    src: 'https://raw.githubusercontent.com/olmeor/momentum-backgrounds/sounds/ME3-Das_Malefitz.mp3',
+    duration: 252,
   },
 ];
 
