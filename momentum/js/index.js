0ba600a8b3c4fec340298916a6cab3a2d5c88227
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-20 18:21:23 +0700

    feat: add todo CRUD

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -43,13 +43,123 @@ initAudio();
 import initSettings from './settings.js';
 initSettings();
 
-const todoMenu = document.querySelector('.todo');
+// ToDo open / close
+
+const todoMenu = document.querySelector('.todo-wrapper');
 const todoButton = document.querySelector('.todo-button');
+const todoButtonClose = document.querySelector('.todo-close-button');
+const todoInputButton = document.querySelector('.todo-input-button');
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
+let editId = false;
 
 function toggleTodoMenu() {
-  todoMenu.classList.toggle('todo-close'); //disable-block
-  // settingsMenu.classList.toggle('settings-menu-shadow');
+  todoMenu.classList.toggle('todo-close');
 }
 
 todoButton.addEventListener('click', toggleTodoMenu);
+todoButtonClose.addEventListener('click', toggleTodoMenu);
 
+let todos = [];
+
+function checkKeypressEnter(event) {
+  if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+    addTodo();
+    event.preventDefault();
+  }
+}
+
+function addTodo() {
+  if (editId) {
+    todos[editId].text = todoInput.value;
+    editId = false;
+    renderTodo();
+    todoInput.value = '';
+    return;
+  }
+  const text = todoInput.value;
+  const todo = {
+    text,
+    done: 'active',
+    id: `${Math.random()}`,
+  };
+  todos.push(todo);
+  todoInput.value = '';
+  renderTodo();
+  console.log(todos);
+}
+
+function deleteTodo(id) {
+  todos.forEach(todo => {
+    if (todo.id === id) {
+      todo.done = 'deleted';
+    }
+  })
+  renderTodo();
+}
+
+function completeTodo(id) {
+  todos.forEach(todo => {
+    if (todo.id === id) {
+      todo.done = 'completed';
+    }
+  })
+  renderTodo();
+}
+
+function renderTodo() {
+  let todoItem = '';
+
+  todos.forEach(todo => {
+    if (todo.done == 'deleted' || todo.done == 'completed') {
+      return;
+    };
+
+    todoItem += `
+      <div class="todo-item">
+        <input id="${todo.id}" type="checkbox" class="todoCheckbox" value="active">
+        <span class="userTodo" contentEditable="false">${todo.text}</span>
+        <button class="todo-item-done"></button>
+        <button class="todo-item-edit"></button>
+        <button  class="todo-item-delete"></button>
+      </div>
+    `;
+  })
+
+  todoList.innerHTML = todoItem;
+
+}
+
+todoInputButton.addEventListener('click', addTodo);
+todoInput.addEventListener('keypress', checkKeypressEnter);
+
+todoList.addEventListener('click', (event) => {
+  if (event.target.classList.contains("todo-item-delete")) {
+    const id = event.target.parentNode.firstElementChild.id;
+    deleteTodo(id);
+  }
+
+  if (event.target.classList.contains("todo-item-done")) {
+    const id = event.target.parentNode.firstElementChild.id;
+    completeTodo(id);
+  }
+
+  if (event.target.classList.contains("todo-item-edit")) {
+    const id = event.target.parentNode.firstElementChild.id;
+    editTodo(id);
+  }
+})
+
+function editTodo(id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id) {
+      editId = i;
+      console.log(editId);
+      todoInput.value = todos[i].text;
+      todoInput.focus();
+      return;
+    }
+  }
+}
+
+renderTodo();
